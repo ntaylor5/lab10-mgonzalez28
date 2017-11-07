6a9cfeac10d2f5af8634c948a97eaf1cc44c03b9
Miguel Gonzalez <mgonzalez28@luc.edu>
2017-11-07 22:45:47 +0000

    Updates to WordCounter

--- src/main/java/edu/luc/cs271/wordcount/DescendingByCount.java
+++ src/main/java/edu/luc/cs271/wordcount/DescendingByCount.java
@@ -7,8 +7,9 @@ import java.util.Map;
 public class DescendingByCount implements Comparator<Map.Entry<String, Integer>> {
   public int compare(final Map.Entry<String, Integer> l, final Map.Entry<String, Integer> r) {
 
-    // TODO turn this into descending order using the getValue() method on l and r
-    return -1;
+    // DONE turn this into descending order using the getValue() method on l and r
+    
+    return r.getValue().compareTo(l.getValue());
 
   }
 }
--- src/main/java/edu/luc/cs271/wordcount/Main.java
+++ src/main/java/edu/luc/cs271/wordcount/Main.java
@@ -11,32 +11,30 @@ public class Main {
 
     // TODO complete this main program
     // 1. create a WordCounter instance
+    Map<String, Integer> map = new HashMap<>();
+    WordCounter counter = new WordCounter(map);
     // 2. use this to count the words in the input
+    WordCounter.countWords(input);
     // 3. determine the size of the resulting map
+    int size = counter.size();
     // 4. create an ArrayList of that size and
     // 5. store the map's entries in it (these are of type Map.Entry<String, Integer>
+    List<Map.Entry<String, Integer>> entries = new ArrayList<>(size);
+     entries.addAll(counter.entrySet());
     // 6. sort the ArrayList in descending order by count
     //    using Collections.sort and an instance of the provided comparator (after fixing the latter)
+   Collections.sort(entries, new DescendingByCount());
     // 7. print the (up to) ten most frequent words in the text
-    Map<String, Integer> map = new HashMap<>();
-   List<counter> list = new ArrayList<counter>();
-   WordCounter counter = new WordCounter();
-   
-   while(input.hasNext()) {
-     String key = input.nextLine();
-     if (key != null) {
-       list.add(key);
-       map.put(key, 0);
-     }
-   }
-   
-   Iterator itr = list.iterator();
-   counter.countWords(itr);
-   
-   
-  
-   
-   
-
+    
+    if (couner.size() < 11) {
+      for (int i = 0; i < counter.size(); i++) {
+        System.out.println(entries(i));
+      }
+      else {
+        for (int j = 0; j < counter.size(); j++) {
+          System.out.println(entries.get(i));
+        }
+      }
+    }
   }
 }
