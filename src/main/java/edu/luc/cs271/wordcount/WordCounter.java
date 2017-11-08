74370b61eae3383cbb8c8876370defd3fefa1c6f
Nick Taylor <31541603+ntaylor5@users.noreply.github.com>
2017-11-07 23:42:54 -0600

    Final Update

--- src/main/java/edu/luc/cs271/wordcount/WordCounter.java
+++ src/main/java/edu/luc/cs271/wordcount/WordCounter.java
@@ -13,37 +13,57 @@ public class WordCounter {
   /** Creates a word counter instance based on the given map. */
   public WordCounter(final Map<String, Integer> theMap) {
 
+    // TODO
     // DONE
     this.theMap = theMap;
+
   }
 
   /** Counts the frequencies of all words in the given iterator. */
   public void countWords(final Iterator<String> words) {
 
-    // DONE for each word in the iterator, update the corresponding frequency in the map
+    // TODO for each word in the iterator, update the corresponding frequency in the map
     // HINT to do this without a conditional, use the getOrDefault method
-    int count = 0;
+
+    // DONE, but check later
+
+    int counter = 0;
     while (words.hasNext()) {
-      String temp = words.next();
-      temp.toLowerCase();
-      if (theMap.containsKey(temp)) {
-        count = theMap.get(temp);
-        theMap.put(temp, count + 1);
-      } else theMap.put(temp, 1);
+      String str = words.next();
+      str.toLowerCase();
+      if (theMap.containsKey(str)) {
+        counter = theMap.get(str);
+        theMap.put(str, counter + 1);
+      }
+      else theMap.put(str, 1);
+
+    //while (words.hasNext()) {
+    //  String str = words.next();
+    //  str = str.toLowerCase();
+    //  Integer someInt = this.theMap.getOrDefault(str, 0);
+    //  this.theMap.put(str, someInt++);
+
+
     }
   }
 
   /** Retrieve the frequency of a particular word. */
   public int getCount(final String word) {
 
+    // TODO
     // DONE
     int count = 0;
     if (theMap.containsKey(word)) {
       count = theMap.get(word);
       theMap.put(word, count);
-    } else theMap.put(word, 0);
+    }
+    else {
+      theMap.put(word, 0);
+    }
     return count;
   }
+
+
 
   /** Retrieve the map representing all word frequencies. */
   public Map<String, Integer> getCounts() {
